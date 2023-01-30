f876b55ad7d325575ebab4abfaaf10d4714659b9
httplex <desufg826@gmail.com>
2023-01-30 16:13:11 -0300

    novas classes adicionadas

---
+++ src/br/com/dio/desafio/Main.java
@@ -0,0 +1,30 @@
+package br.com.dio.desafio;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso JAVA");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso JavaScript");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição monitoria Java");
+        mentoria.setData(LocalDate.now());
+
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+    }
+}
---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,54 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo, descricao;
+    private int cargaHoraria;
+    private LocalDate data;
+
+    public Mentoria() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                ", data=" + data +
+                '}';
+    }
+}
